50f3e40b51d81eaab650f3d922d52a8aca2005d0
tonezone108 <tonezone108@gmail.com>
2020-02-13 20:27:46 -0600

    Done

--- src/Router.js
+++ src/Router.js
@@ -1,27 +1,39 @@
-import React from 'react'
-import { Switch, Route, Redirect } from 'react-router'
-import cookie from 'cookie'
-import Home from './components/Home'
-import About from './components/About'
-import Car from './components/Car'
-import Login from './components/Login'
+import React from "react";
+import { Switch, Route, Redirect } from "react-router";
+import cookie from "cookie";
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car";
+import Login from "./components/Login";
 
 // Write checkAuth function here
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie);
+  return cookies["loggedIn"] ? true : false;
+};
 // Check the cookies for a cookie called "loggedIn"
 
-
 // Write ProtectedRoute function here
-
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        checkAuth() ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
 
 const Router = () => {
-    return (
-        <Switch>
-            <Route path="/login" component={Login} />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/car/:id" component={Car} />
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute path="/about" component={About} />
+      <ProtectedRoute path="/car/:id" component={Car} />
+    </Switch>
+  );
 };
 
 export default Router;
